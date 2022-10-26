fc10715ca6b1d709f05f63532f9c0a22957ba169
Md. Saiful Islam <saiful9122113@gmail.com>
2022-10-26 21:58:38 +0600

    add courses tab sidebar

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import './courses.css';
 
 const Courses = () => {
+    const coursesData = useLoaderData();
+
     return (
-        <div>
-            <h3>Courses part</h3>
-        </div>
+        <section className='courses'>
+            <div className="sidebar">
+                <h5>Course Title</h5>
+                {coursesData && coursesData.map(course => <h6>{course.name}</h6>)}
+            </div>
+            <div className="mainContainer">
+            </div>
+        </section>
     );
 };
 
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Blog from "../Pages/Blog/Blog";
+import Course from "../Pages/Courses/Course";
+import Courses from "../Pages/Courses/Courses";
 import Home from "../Pages/Home/Home";
 
 export const routes =createBrowserRouter([
@@ -19,6 +21,20 @@ export const routes =createBrowserRouter([
             {
                 path:'/blog/:id',
                 element:<Blog></Blog>
+            },
+            {
+                path:'/courses',
+                loader: ({params}) => {
+                    return fetch(`http://localhost:5000/courses`)
+                },
+                element:<Courses></Courses>
+            },
+            {
+                path:'/course/:id',
+                loader: ({params}) => {
+                    return fetch(`http://localhost:5000/course/${params.id}`)
+                },
+                element:<Course></Course>
             },
         ]
     }
